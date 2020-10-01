cfb9455bc40f10ca75be4aa93a41affbb2e1fa20
maharajan <maharajan@aktrea.com>
2020-10-02 00:33:17 +0530

    update app js

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use(
 app.use("/gallery", express.static(path.join(__dirname, "gallery")));
 app.use(express.static(path.join(__dirname)));
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "Views"));
+app.set("views", "views");
 app.use("/", router);
 const port = process.env.PORT || 3000;
 app.listen(port);
