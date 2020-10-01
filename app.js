84969e65ef93a527806f593c4aab1c573e544998
maharajan <maharajan@aktrea.com>
2020-10-02 01:18:45 +0530

    up

--- app.js
+++ app.js
@@ -44,10 +44,10 @@ app.use(
 );
 app.use("/gallery", express.static(path.join(__dirname, "gallery")));
 app.use(express.static(path.join(__dirname)));
-app.set("views", "views");
+app.set("views", "/views");
 app.set("view engine", "ejs");
 
-app.use("/User", router);
+app.use("/", router);
 app.get("/", (req, res, next) => {
   res.send("<h2>welcome to gallery</h2>");
 });
